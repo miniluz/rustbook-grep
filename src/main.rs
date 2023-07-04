59b3d048005112ee1f6f27166794f4bff1e3c44c
miniluz <javiermelon4fu@gmail.com>
2023-07-05 00:00:48 +0200

    Modularization through lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
+
+use rustbook_grep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,25 +14,8 @@ fn main() {
     dbg!(&config.query);
     dbg!(&config.file_path);
 
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-
-    println!("With text:\n{contents}");
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
+    if let Err(e) = rustbook_grep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
     }
 }
